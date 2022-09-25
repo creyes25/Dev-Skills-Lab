de115a78fd34e390568875bfe946bd748210dc4c
creyes25 <carolina.reyes2022@gmail.com>
2022-09-25 03:03:14 -0500

    Add a functionality to render a page where you add a new skill

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,23 @@
-import { skills } from '../data/skills-data.js'
+import { Skill } from '../models/skill.js'
 
 function index(req, res) {
-  res.render('skills/index', {
-    skills: skills
+  Skill.find({})
+  .then(skills => {
+    res.render('skills/index', {
+      skills: skills
+    })
+  }).catch(error => {
+    console.lot(error)
+    res.redirect('/')
   })
 }
 
+// render a view page where the new skill will be added
+function newSkill(req, res) {
+  res.render('skills/new')
+}
+
 export {
-  index
+  index,
+  newSkill as new
 }
--- routes/skills.js
+++ routes/skills.js
@@ -6,6 +6,8 @@ const router = Router()
 /* GET skills listing. */
 router.get('/', skillsCtrl.index)
 
+router.get('/new', skillsCtrl.new)
+
 export {
   router
 }
